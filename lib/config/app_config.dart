f72d49fa50993a312a219d4a2e5c2f228ad9143f
Miku Nakano <19827@Q0oIzjqui3.Q0oIzjqui3>
2024-09-15 20:47:53 +0800

    first commit

---
+++ lib/config/app_config.dart
@@ -0,0 +1,20 @@
+// Enter your Agora AppId for calling
+const agoraAppId = "00000000000000000000000000000";
+// add your privacy policy url
+const String termConditionUrl =
+    "https://www.deligence.com/apps/hookup4u/Terms-Service.html";
+const String privacyUrl =
+    "https://www.deligence.com/apps/hookup4u/Privacy-Policy.html";
+
+const bannerAdUnitIdAndriod = '000000000000000000000000000000000';
+const interstitialAdUnitIdAndriod = "ca-app-pub-7622543861174815~5424386600";
+const rewardedAdUnitIdAndriod = "ca-app-pub-7622543861174815~5424386600";
+// Add google map key for google places search
+const googleMapsKey = 'AIzaSyAnuSVD9iUKXlFc5U0V875qSuiYmrcWcd4';
+//for support to user add you mail
+const adminMail = "admin@gmail.com";
+// add bucket id from firebase or google-services-json
+const String bucketId = "just-date-f9052.appspot.com";
+//for pagination set limit
+
+const int perPageData = 20;
---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,46 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import agora_rtc_engine
+import cloud_firestore
+import cloud_functions
+import file_selector_macos
+import firebase_auth
+import firebase_core
+import firebase_messaging
+import firebase_storage
+import flutter_image_compress_macos
+import in_app_purchase_storekit
+import iris_method_channel
+import location
+import package_info_plus
+import path_provider_foundation
+import share_plus
+import shared_preferences_foundation
+import sqflite
+import webview_flutter_wkwebview
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AgoraRtcNgPlugin.register(with: registry.registrar(forPlugin: "AgoraRtcNgPlugin"))
+  FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
+  FLTFirebaseFunctionsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFunctionsPlugin"))
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
+  FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
+  FlutterImageCompressMacosPlugin.register(with: registry.registrar(forPlugin: "FlutterImageCompressMacosPlugin"))
+  InAppPurchasePlugin.register(with: registry.registrar(forPlugin: "InAppPurchasePlugin"))
+  IrisMethodChannelPlugin.register(with: registry.registrar(forPlugin: "IrisMethodChannelPlugin"))
+  LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
+  FLTWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "FLTWebViewFlutterPlugin"))
+}
